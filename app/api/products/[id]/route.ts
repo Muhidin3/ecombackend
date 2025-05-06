add1fa48cb1d9c3dfbd5411cdff131c282355e38
Muhidin3 <muhidinshemsu3@gmail.com>
2025-05-06 15:12:26 +0300

    testing eachproduct

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -29,9 +29,10 @@ export async function GET(req:NextRequest,{params}: {params: Promise<{ id: strin
     try {
         const res = await Product.findById(id)
         const user = await User.findById(res.user)
-        const data = {...res}
+        const data = {...res}._doc
         data.user = user.name
         data.userid = user._id
+        console.log(data)
         return NextResponse.json({message:'got it',data})
         
     } catch (error) {
